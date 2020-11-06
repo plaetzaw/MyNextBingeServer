d0550e8d1929bf36997f868fa3b01206f268284c
plaetzaw <plaetzaw@gmail.com>
2020-11-06 13:38:13 -0600

    Updated Config with .ENV variables

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+module.exports = {
+    "development": {
+      "username": process.env.USERNAME,
+      "password": process.env.PASSWORD,
+      "database": process.env.DATABASE,
+      "host": process.env.HOST,
+      "dialect": "postgres"
+    },
+    "production": {
+      "username": process.env.USERNAME,
+      "password": process.env.PASSWORD,
+      "database": process.env.DATABASE,
+      "host": process.env.HOST,
+      "dialect": "postgres"
+    }
+  }
+
+
+
+//USE SECRETS
